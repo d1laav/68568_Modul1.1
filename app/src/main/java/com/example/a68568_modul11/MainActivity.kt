111a6d0c1923737ac8e23cf5c0ecc0c2d25be799
d1laav <dimmanuel31@gmail.com>
2025-02-17 19:53:03 +0700

    3. Tweaking the UI

--- app/src/main/java/com/example/a68568_modul11/MainActivity.kt
+++ app/src/main/java/com/example/a68568_modul11/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a68568_modul11.ui.theme._68568_Modul11Theme
 
 class MainActivity : ComponentActivity() {
@@ -43,3 +45,4 @@ fun GreetingPreview() {
         Greeting(name = "Android")
     }
 }
+
